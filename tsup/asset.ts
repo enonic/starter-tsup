df20e9630dbab609cad6feb4b3e14f58b6390de6
Christian Westgaard <comlock@gmail.com>
2023-05-22 15:26:35 +0200

    Whitespace

--- tsup/asset.ts
+++ tsup/asset.ts
@@ -8,35 +8,35 @@ import { DIR_SRC_ASSETS } from './constants';
 
 export default function buildAssetConfig(): Options {
 	const GLOB_EXTENSIONS_ASSETS = '{tsx,ts,jsx,js}';
-		const FILES_ASSETS = globSync(`${DIR_SRC_ASSETS}/**/*.${GLOB_EXTENSIONS_ASSETS}`);
-		// print(FILES_ASSETS, { maxItems: Infinity });
-		return {
-			bundle: true, // Needed to bundle @enonic/js-utils and dayjs
-			dts: false, // d.ts files are use useless at runtime
-			entry: FILES_ASSETS,
-			esbuildPlugins: [],
+	const FILES_ASSETS = globSync(`${DIR_SRC_ASSETS}/**/*.${GLOB_EXTENSIONS_ASSETS}`);
+	// print(FILES_ASSETS, { maxItems: Infinity });
+	return {
+		bundle: true, // Needed to bundle @enonic/js-utils and dayjs
+		dts: false, // d.ts files are use useless at runtime
+		entry: FILES_ASSETS,
+		esbuildPlugins: [],
 
-			// By default tsup bundles all imported modules, but dependencies
-			// and peerDependencies in your packages.json are always excluded
-			external: [ // Must be loaded into global scope instead
-			],
+		// By default tsup bundles all imported modules, but dependencies
+		// and peerDependencies in your packages.json are always excluded
+		external: [ // Must be loaded into global scope instead
+		],
 
-			format: [
-				// 'cjs', // Legacy browser support
-				'esm',
-			],
-			minify: process.env.NODE_ENV === 'development' ? false : true,
+		format: [
+			// 'cjs', // Legacy browser support
+			'esm',
+		],
+		minify: process.env.NODE_ENV === 'development' ? false : true,
 
-			// TIP: Command to check if there are any bad requires left behind
-			// grep -r 'require("' build/resources/main | grep -v 'require("/'|grep -v chunk
-			noExternal: [
-				/^@enonic\/js-utils/,
-			],
+		// TIP: Command to check if there are any bad requires left behind
+		// grep -r 'require("' build/resources/main | grep -v 'require("/'|grep -v chunk
+		noExternal: [
+			/^@enonic\/js-utils/,
+		],
 
-			platform: 'browser',
-			silent: ['QUIET', 'WARN'].includes(process.env.LOG_LEVEL_FROM_GRADLE||''),
-			splitting: true,
-			sourcemap: process.env.NODE_ENV === 'development' ? false : true,
-			tsconfig: 'src/main/resources/assets/tsconfig.json',
-		};
+		platform: 'browser',
+		silent: ['QUIET', 'WARN'].includes(process.env.LOG_LEVEL_FROM_GRADLE||''),
+		splitting: true,
+		sourcemap: process.env.NODE_ENV === 'development' ? false : true,
+		tsconfig: 'src/main/resources/assets/tsconfig.json',
+	};
 }
