de778999a467da836beede58ff47952556b0ba4e
Christian Westgaard <comlock@gmail.com>
2023-05-26 10:49:30 +0200

    Get coverage on untested files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+	collectCoverageFrom: [
+		'src/main/resources/**/*.{ts,tsx}'
+	],
+	coverageProvider: 'v8',
 	preset: 'ts-jest/presets/js-with-babel-legacy',
 	testEnvironment: 'node',
 };
