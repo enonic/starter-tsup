6a1ae33bc8eda6293e193658f8824553683b2737
alansemenov <ase@enonic.com>
2023-08-15 11:10:58 +0200

    Removed '@ts-expect-error' directive for Task API calls

--- src/main/resources/main.ts
+++ src/main/resources/main.ts
@@ -5,7 +5,7 @@ import {DEBUG_MODE} from "/constants"; // Using relative, so it will be inlined 
 DEBUG_MODE && log.info('main: Hello from transpiled typescript server-side code.');
 
 // Test that descontruction works after transpilation.
-const obj = { prop: 'Decontruction works :)' };
+const obj = { prop: 'Deconstruction works :)' };
 const { prop } = obj;
 DEBUG_MODE && log.info(prop);
 
@@ -13,12 +13,10 @@ importedFunction();
 
 submitTask({
 	descriptor: 'testRepoConnectionQuery',
-	// @ts-expect-error: TS2345: Argument of type '{ descriptor: string; name: string; }' is not assignable to parameter of type 'SubmitTaskParams<Record<string, unknown>>'.
 	name: `${app.name}:testRepoConnectionQuery:nameA`
 })
 submitTask({
 	descriptor: 'testRepoConnectionQuery',
-	// @ts-expect-error: TS2345: Argument of type '{ descriptor: string; name: string; }' is not assignable to parameter of type 'SubmitTaskParams<Record<string, unknown>>'.
 	name: `${app.name}:testRepoConnectionQuery:nameA`
 });
 // submitTask({ descriptor:'testContentQuery' });
