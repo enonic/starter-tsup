ff8ff99550e4d2eaf02c46576c194044e74c8a09
alansemenov <ase@enonic.com>
2023-08-16 10:09:14 +0200

    placeholder image "leaks" through the admin tool logo #105

--- src/main/resources/static/admin/App.tsx
+++ src/main/resources/static/admin/App.tsx
@@ -6,22 +6,20 @@ import dayjs from 'dayjs';
 import React from 'react';
 import { useId } from 'react';
 
-
 export function App() {
 	const id = useId();
 	console.debug(`Hello from App.tsx. React app id: ${toStr({id})}`);
 	return (
 		<>
 			<div className="appbar">
-				<div className="home-button app-icon system-info-button">
-					<img alt="Application Icon" className="app-icon"/>
+				<div className="home-button app-icon">
 					<span className="app-name">Sample Tool</span>
 				</div>
 			</div>
 			<div className="admin-tool-container">
-				<h3 className="darkgray">This is a sample Admin Tool. Note the XP menu icon in the top right corner.</h3>
+				<h3 className="darkgray">This is a React app inside an Admin Tool. Note the XP menu icon in the top right corner.</h3>
 				<br/>
-				<h5 className="lightgray">Hello from Day.js! Current time is: {dayjs().format('DD.MM.YYYY HH:MM:ss')}</h5>
+				<h5 className="lightgray">Hello from Day.js! Current date/time is: {dayjs().format('DD.MM.YYYY H:mm:ss [GMT]Z')}</h5>
 			</div>
 		</>
 	);
