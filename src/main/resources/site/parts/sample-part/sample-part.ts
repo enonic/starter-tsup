abb4894781bf6954a2a8f3d610f1b767f88f298f
alansemenov <ase@enonic.com>
2023-08-30 15:09:49 +0200

    Improve the look of page components #107

--- src/main/resources/site/parts/sample-part/sample-part.ts
+++ src/main/resources/site/parts/sample-part/sample-part.ts
@@ -7,12 +7,14 @@ import type {
 //@ts-ignore
 import {render} from '/lib/thymeleaf';
 import {getComponent} from '/lib/xp/portal';
+import {DEBUG_MODE} from '/constants';
 
 
-const VIEW = resolve('./examplePart.html');
+const VIEW = resolve('./sample-part.html');
 
 
 export function get(/*request: Request*/): Response {
+	DEBUG_MODE && log.info('Hello from the part controller!');
 	const {
 		config: {
 			myProperty = 'Fallback text'
