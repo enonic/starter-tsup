93050d2e3709c084ef9033cb20ac4179a341b9d6
alansemenov <ase@enonic.com>
2023-12-05 15:23:54 +0100

    style -> styles

--- src/main/resources/admin/widgets/menuItem/menuItem.ts
+++ src/main/resources/admin/widgets/menuItem/menuItem.ts
@@ -1,10 +1,12 @@
 import type {
 	Response,
 } from '/index.d';
+import { assetUrl } from '/lib/xp/portal';
 
 export function get(): Response {
 	return {
-		body: `<widget style="margin-top: -44px;height: 100vh;display: flex;align-items: center;justify-content: center;">
+		body: `<widget id="widget-menuitem">
+					<link rel="stylesheet" href="${assetUrl({ path: '/styles/widget.css' })}" type="text/css" media="all"/>
 					<h6>This is a Menu Widget. It can be opened from the menu on the left-hand side.</h6>
 				</widget>`,
 		contentType: 'text/html'
